7da85ebe4590770a37eee4acfab6525d9c84f15a
sebbeOMG <sebb.sebbe@gmail.com>
2014-01-27 17:36:29 +0100

    Working on Form validation

--- project/client.js
+++ project/client.js
@@ -4,7 +4,35 @@ var displayView = function() {
 
 window.onload = function() {
     //Runs when page is loaded
-    var temp = document.getElementById("welcomeView").innerHTML;
-    console.log(temp);
-    document.getElementById("view").innerHTML += temp;
+    document.getElementById("view").innerHTML += document.getElementById("welcomeView").innerHTML;
 };
+
+var signUp = function() {
+    return validateSignUp();
+}
+
+var validateSignUp = function() {
+    var inputs = document.getElementById("signup").getElementsByTagName("input");
+    var noError = true;
+    for (var i = inputs.length - 1; i >= 0; i--) {
+        if (inputs[i].value == "") {
+            inputs[i].style.borderColor = "red";
+            noError = false;
+        }
+    };
+    if (!noError) {
+        if (inputs["psw"].value != inputs["rpsw"].value) {
+            inputs["psw"].style.borderColor = "red";
+            inputs["rpsw"].style.borderColor = "red";
+            inputs["psw"].value = "";
+            inputs["rpsw"].value = "";
+            noError = false;
+        }
+    }
+    console.log(noError);
+    return noError;
+}
+
+var clearit = function() {
+	console.log("hej");
+}
