9763499f1d4f86a0b4e4284eb8d3d510cd0522b9
Anton Forsberg <anton.forsberg1@gmail.com>
2014-02-10 11:34:31 +0100

    some code

--- project_2/database_helper.py
+++ project_2/database_helper.py
@@ -1,8 +1,18 @@
 import sqlite3
 
-conn = sqlite3.connect('database.db')
-curs = conn.cursor()
+
 
 def addUser():
+	conn = sqlite3.connect('database.db')
+	curs = conn.cursor()
 	curs.execute('')
 	conn.commit()
+
+def getUser(email):
+	conn = sqlite3.connect('database.db')
+	curs = conn.cursor()
+	command = 'SELECT * FROM USER AS U WHERE U.email = ?'
+	userInfo = []
+	userInfo = curs.execute(command, email)
+	conn.close()
+	return userInfo
--- project_2/server.py
+++ project_2/server.py
@@ -3,32 +3,53 @@ import sqlite3
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+	return 'This is a front page, looking beautiful'
+
+@app.route('/signin')
 def signIn(email, password):
 	#Authenticates user, returns string containing random generated token
-
+	pass
+@app.route('/signup')
 def signUp(email, password, firstname, familyname, gender, city, country):
 	#Registers user in database
-
+	return 'you just signed up'
+	
+@app.route('/signout')
 def signOut(token):
 	#Signs a user out
+	pass
 
+@app.route('/changepassword')
 def changePassword(token, oldPass, newPass):
 	#Changes a users password in the database
+	pass
 
+@app.route('/getuserdata')
 def getUserDataByToken(token):
 	#Returns a user object containing all info
+	pass
 
+@app.route('/getuserdataemail')
 def getUserDataByEmail(token, email):
 	#Returns a user object
+	pass
 
+@app.route('/getmessagetoken')
 def getUserMessagesByToken(token):
 	#Returns an array containing all messages sent to user
+	pass
 
+@app.route('/getmessageemail')
 def getUserMessagesByEmail(token, email):
 	#Same as above for the email-user
+	pass
 
+@app.route('/postmessage')
 def postMessage(token, message, email):
 	#Post a message
+	pass
 
-
-
+if __name__=='__main__':
+	app.run()
